b15ed4a8ad0081f13155e92dabeabcb474ebad6b
analauramolina <almm95@gmail.com>
2020-09-01 19:41:55 -0300

    con test de suma

--- calc/tests.py
+++ calc/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get("http://127.0.0.1:8000/calc/")
+
+#Identificar elementos usar box para campos a completar y button para botones
+entrada_box = driver.find_element_by_name("query")
+#Acción con elementos
+entrada_box.send_keys("2+2")
+entrada_box.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+#submit_button.click()
+#resultado_box = driver.find_element_by_name("query")
+resultado_box = driver.find_element_by_name("resultado")
+valor = resultado_box.get_attribute("value")
+assert valor == "2+2=4"
+#time.sleep(5)
+driver.quit()
