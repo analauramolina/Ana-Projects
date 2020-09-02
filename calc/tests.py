679d81960da7f67ac2ba0a9528811a16bb5f8394
analauramolina <almm95@gmail.com>
2020-09-01 21:40:29 -0300

    3+3

--- calc/tests.py
+++ calc/tests.py
@@ -9,13 +9,13 @@ driver.get("http://127.0.0.1:8000/calc/")
 #Identificar elementos usar box para campos a completar y button para botones
 entrada_box = driver.find_element_by_name("query")
 #Acción con elementos
-entrada_box.send_keys("2+2")
+entrada_box.send_keys("3+3")
 entrada_box.send_keys(Keys.RETURN)
 #assert "No results found." not in driver.page_source
 #submit_button.click()
 #resultado_box = driver.find_element_by_name("query")
 resultado_box = driver.find_element_by_name("resultado")
 valor = resultado_box.get_attribute("value")
-assert valor == "2+2=4"
+assert valor == "3+3=6"
 #time.sleep(5)
 driver.quit()
